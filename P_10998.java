374fdb89fc59f6b531cf75cb7cb389d442c8b7f5
LeeMinJun <lmjun777@gmail.com>
2021-02-25 23:22:21 +0900

    Create P_10998.java

---
+++ P_10998.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P_10998 {
+    public static void main(String[] args) {
+        int a, b;
+
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        System.out.println(a * b);
+    }
+}
